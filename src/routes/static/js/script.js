325297349e4564d098b6662a148d420357dbe044
ChristopherCautivar <ccautivar@csumb.edu>
2021-07-09 00:50:56 -0700

    minor formatting improvements

--- src/routes/static/js/script.js
+++ src/routes/static/js/script.js
@@ -9,16 +9,6 @@ function makeEntityElement(entity, editFields){
     return result;
 }
 
-function makeElement(cssClass, id, content){
-    // makes the desired element... well in the first place we already have objects
-    // so this function should simply create a visual representation of such an object
-    // therfore: cssClass is solely in charge of applying the correct css rules to the element
-    // via a combination fo user defined styling and object-style-rule pairs.
-    // id carries over from the database, adjusted for the first id called
-    // content is built with a combination of makeElement calls and the content stored in database
-    // too granular for use? or useful for specific cases, such as simple elements
-    return `<div class="${cssClass}" id="${id}">${content}</div>`
-}
 function makeGroup(group){
     // not every todo will belong to a group
     // append group info
@@ -90,50 +80,51 @@ function makeEntityElement(entity, id, editFields){
     // which cannot be found currently...
     result = "<div class='container-fluid todo'>"
     result += `<input type='hidden' id='id${id}' name='id${id}' value='${id}'>`;
+    result += `<ul>`;
     for(var property in entity){
         // private properties are not included in this for loop
         // if editFields, also return the corresponding form or input type to allow the user to submit an ajax call
         // to create/update the field. ex. textarea or datepicker
-        if(editFields || entity[property] != 0 || entity[property] === false){
-            result += `${property}: `
+        if(editFields || entity[property] != 0 || property == "completed"){
+            result += `<li>${capitalizeFirst(property)}: `
         }
-        if(editFields){
+        if(editFields || property == "completed"){
             // if working on an existing element, put values
             // otherwise a default todo will be passed, making their values effectively the desired defaults
             // might need some span tags for better formatting
             switch(property){
                 case "title":
-                    result += `&nbsp<input type='text' id='title${id}' name='title${id}' value='${entity[property]}'>`
+                    result += `&nbsp<input type='text' id='title${id}' name='title${id}' value='${entity[property]}'></li>`
                     break;
                 case "description":
-                    result += `&nbsp<textarea id='description${id}' name='description${id}'>${entity[property]}</textarea>`
+                    result += `&nbsp<textarea id='description${id}' name='description${id}'>${entity[property]}</textarea></li>`
                     break;
                 case "completed":
-                    result += `&nbsp<input type='checkbox' id='completed${id}' name='completed${id}' value='isChecked' ${(Boolean(entity[property]) ? "checked=''" : "")}>`
+                    result += `&nbsp<input type='checkbox' id='completed${id}' name='completed${id}' value='isChecked' ${(Boolean(entity[property]) ? "checked=''" : "")}></li>`
                     break;
                 case "weight":
-                    result += `&nbsp<input type='number' id='weight${id}' name='weight${id}' min='0' max='10' value='${entity[property]}'>`
+                    result += `&nbsp<input type='number' id='weight${id}' name='weight${id}' min='0' max='10' value='${entity[property]}'></li>`
                     break;
                 case "time_estimate":
-                    result += `&nbspHours: <input type='number' id='time_estimate_hours${id}' name='time_estimate_hours${id}' min ='0' max='24' value='${entity[property]/60}'>` + 
-                        `&nbspMinutes: <input type='number' id='time_estimate_minutes${id}' name='time_estimate_minutes${id}' min ='0' max='60' value='${entity[property]%60}'>`
+                    result += `&nbspHours: <input type='number' id='time_estimate_hours${id}' name='time_estimate_hours${id}' min ='0' max='24' value='${entity[property] != 0 ? entity[property]/60 : ""}'>` + 
+                        `&nbspMinutes: <input type='number' id='time_estimate_minutes${id}' name='time_estimate_minutes${id}' min ='0' max='60' value='${entity[property] != 0 ? entity[property]%60 : ""}'></li>`
                     break;
                 case "due_date":
                     // datetime
                     var today = new Date()
+                    // value of form 2022-07-01
                     result += `&nbsp<input type='date' id='due_date${id}' name='due_date${id}' min='${today.getFullYear()}-${
-                        String(today.getMonth()+1).padStart(2,"0")}-${String(today.getDate()).padStart(2,"0")}' value="2022-07-01">`
+                        String(today.getMonth()+1).padStart(2,"0")}-${String(today.getDate()).padStart(2,"0")}' value=""></li>`
                     break;
             }
-            result += "<br>"
         } else {
             // just put the value of the property
-            if(editFields || entity[property] != 0 || entity[property] === false){
-                result += `${entity[property]}`
-                result += "<br>"
+            if(editFields || entity[property] != 0 || property == "completed"){
+                result += `${entity[property]}</li>`
             }
         }
     }
+    result += `</ul>`
     if(editFields){
         result += `<button id='submit' onclick='submitTodo()'>Submit</button><br>`
     }
@@ -148,11 +139,11 @@ function submitTodo(){
         data: {id : -1,
             dateCreated : "",
             dateUpdated : "",
-            completed : false,
-            title : "testpost",
-            description : "",
+            completed : 1,
+            title : "test title2",
+            description : "test description",
             tags : [],
-            weight : 0,
+            weight : 5,
             prerequisites : [],
             time_estimate : "",
             due_date : ""},
@@ -160,4 +151,8 @@ function submitTodo(){
             alert(`ID of created entity: ${result}`)
         }
     })
+}
+
+function capitalizeFirst(s){
+    return s.charAt(0).toUpperCase() + s.slice(1);
 }
